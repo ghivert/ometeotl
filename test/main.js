07858398eb34430ea2b287e71bc941f5ccbe7704
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-08-17 20:34:25 +0200

    :recycle: Replace hand made promise by promisify

--- test/main.js
+++ test/main.js
@@ -1,9 +1,10 @@
-const fs = require('./fs')
+const fs = require('fs')
+const util = require('util')
 const parser = require('./parser')
 
 let environment = {}
 
-fs.readFile(process.argv[2])
+util.promisify(fs.readFile)(process.argv[2])
   .then(file => file.toString())
   .then(source => source.replace(/\n/g, ''))
   .then(source => parser.splitSExps(source))
